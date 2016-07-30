0b257d9efcbf020a6c6a44892f6b624074ae7c60
unknown <>
2016-07-30 13:41:59 +0800

    注册Toast

--- static/login_register/js/login_register.js
+++ static/login_register/js/login_register.js
@@ -2,9 +2,9 @@ var lr = angular.module('login-register',['ngMaterial','ngMessages']);
 
 lr.controller("loginCtrl",function($scope,$http){
 	
-	$scope.login = function(){
+	$scope.login = function(username,password){
 		var opt = {
-			url: '/home/login',
+			url: '/accounts/login',
 			method: 'POST',
 			data: JSON.stringify({
 				username: username,
@@ -21,28 +21,62 @@ lr.controller("loginCtrl",function($scope,$http){
 		});
 	};
 	
-}).controller("registerCtrl",function($scope,$http){
-	
-	$scope.register = function(){
+}).controller("registerCtrl",function($scope,$http,$mdToast){
+		
+	$scope.register = function(username,email,password,repassword){
 		var opt = {
-			url: '/home/register',
+			url: '/accounts/register',
 			method: 'POST',
 			data: JSON.stringify({
 				username: username,
 				email: email,
 				password: password,
-				confirm: repassword
+				repassword: repassword
 			}),
 			headers: {'Content-Type': 'application/json'}
 		};
 		$http(opt).success(function(data){
-			if (data.isSuccessful) {
-				$window.location.href = "project_page.html";
+			if (data.successful) {
+				showToast($mdToast,"Register SUCCESSED!");
 			} else{
-				
+				showToast($mdToast,data.error.msg);
 			}
 		});
+		
 	};
 	
 });
 
+var last = {
+	bottom: false,
+	top: true,
+	left: false,
+	right: true
+};
+
+var toastPosition = angular.extend({},last);
+
+var sanitizePosition = function(){
+	var current = toastPosition;
+	if (current.bottom && last.top) current.top = false;
+	if (current.top && last.bottom) current.bottom = false;
+	if (current.left && last.right) current.right = false;
+	if (current.right && last.left) current.left = false;
+	last = angular.extend({},current);
+};
+
+var getToastPosition = function(){
+	sanitizePosition();
+	return Object.keys(toastPosition)
+		.filter(function(pos){return toastPosition[pos];})
+		.join(' ');
+};
+
+var showToast = function($mdToast,msg){
+	var pinTo = getToastPosition();
+	var toast = $mdToast.simple()
+		.textContent(msg)
+		.highlightAction(true)
+		.position(pinTo);
+	$mdToast.show(toast);
+};
